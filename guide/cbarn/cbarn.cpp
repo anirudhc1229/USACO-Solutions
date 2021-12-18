164f3074837c160c60700113b8dcddcc011815e6
Anirudh Chari <anirudh.chari05@gmail.com>
2021-12-17 18:11:50 -0600

    done

---
+++ guide/cbarn/cbarn.cpp
@@ -0,0 +1,43 @@
+/*
+ID: anirudh71
+TASK: cbarn
+LANG: C++
+*/
+
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+int main() {
+
+    ifstream fin("cbarn.in");
+    ofstream fout("cbarn.out");
+
+    int N;
+    fin >> N;
+    vector<int> rooms;
+    for (int i = 0; i < N; i++) {
+        int room;
+        fin >> room;
+        rooms.push_back(room);
+    }
+
+    int ans = 1000000000;
+
+    for (int i = 0; i < N; i++) {
+        int dist = 0;
+        for (int j = i; j < N + i; j++)
+            dist += rooms[j % N] * (j - i);
+        ans = min(ans, dist);
+        cout << dist << endl;
+    }
+
+    fout << ans << endl;
+
+    return 0;
+
+}
