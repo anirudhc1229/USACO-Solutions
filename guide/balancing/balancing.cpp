cf416f726b3379514e27f47caa1debe94274c33d
Anirudh Chari <anirudh.chari05@gmail.com>
2022-01-01 16:50:44 -0600

    10/10

--- guide/balancing/balancing.cpp
+++ guide/balancing/balancing.cpp
@@ -17,26 +17,38 @@ struct Cow {
     int y;
 };
 
+void getLines(vector<int>& vert, vector<int>& horiz, vector<Cow> cows) {
+    
+    sort(cows.begin(), cows.end(), 
+        [](Cow& c1, Cow& c2) -> bool {return c1.x < c2.x;});
+    for (Cow c : cows) vert.push_back(c.x - 1);
+
+    sort(cows.begin(), cows.end(), 
+        [](Cow& c1, Cow& c2) -> bool {return c1.y < c2.y;});
+    for (Cow c : cows) horiz.push_back(c.y - 1);
+
+}
+
 int main() {
 
     ifstream fin("balancing.in");
     ofstream fout("balancing.out");
 
-    int N;
-    fin >> N;
-    int B;
-    fin >> B;
+    int N, B;
+    fin >> N >> B;
     vector<Cow> cows;
     for (int i = 0; i < N; i++) {
-        Cow c;
-        fin >> c.x >> c.y;
-        cows.push_back(c);
+        int x, y; 
+        fin >> x >> y;
+        cows.push_back({x, y});
     }
 
     int ans = N;
+    vector<int> vert, horiz;
+    getLines(vert, horiz, cows);
 
-    for (int a = 0; a <= B; a += 2) {
-        for (int b = 0; b <= B; b += 2) {
+    for (int a : vert) {
+        for (int b : horiz) {
             vector<int> quadrants{0, 0, 0, 0};
             for (Cow c : cows) {
                 if (c.x > a && c.y > b) quadrants[0]++;
